6325cde169210701790ff274eaea81cd3c4d9127
Crawford Paul <cpaul@louisestreet.com>
2025-06-20 10:58:06 -0400

    edit vercel config

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: adapter()
-	}
-};
-
-export default config;
+	  adapter: adapter({
+		runtime: 'nodejs18.x',
+	  }),
+	},
+  };
+   
+  export default config;
