4554e8ef2df02c39fe06b857bb8c29b55421ef6f
Crawford Paul <cpaul@louisestreet.com>
2025-07-11 12:02:49 -0400

    style watch table

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,5 +11,25 @@ export default {
         extend: {}
     },
 
-    plugins: [typography, forms, containerQueries, daisyui]
+    plugins: [typography, forms, containerQueries, daisyui],
+    
+    daisyui: {
+        themes: [
+            {
+                custom: {
+                    "primary": "#325a9b",
+                    "secondary": "#666",
+                    "accent": "#37cdbe",
+                    "neutral": "#3d4451",
+                    "base-100": "#ffffff",
+                    "base-200": "#f8f9fa",
+                    "base-300": "#e9ecef",
+                    "info": "#3abff8",
+                    "success": "#36d399",
+                    "warning": "#fbbd23",
+                    "error": "#f87272",
+                }
+            }
+        ]
+    }
 } satisfies Config;
