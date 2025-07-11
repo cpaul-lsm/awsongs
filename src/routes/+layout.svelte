46138b0cf5baa2caf6f9bf1927f9b7da0f2a9420
Crawford Paul <cpaul@louisestreet.com>
2025-07-11 11:29:25 -0400

    add spped-insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
     import '../../src/app.css';
     import '../../src/main.css';
+
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+    injectSpeedInsights();
+
 </script>
 
 <section class="app-layout pad-st bg-white">
